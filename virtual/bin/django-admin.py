93b908796f6d57e3ee23a539eaf7efa9ce83edfe
Martin Santa Cruz <mesantacruz@uc.cl>
2020-04-30 11:31:00 -0400

    api3

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/egidio17/Escritorio/T2TallerIntegracion_MartinSantaCruz/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
